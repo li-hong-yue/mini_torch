597a062c2f21f588c85d4b26f924b58449cf9287
Hongyue Li <lhy@stanford.edu>
2025-10-01 20:17:38 -0700

    update

--- mini_torch/__init__.py
+++ mini_torch/__init__.py
@@ -1,11 +1,11 @@
 # mini_torch/__init__.py
-from .tensor import Tensor, _grad_broadcast
+from .tensor import Tensor
 from .functions import sigmoid, relu, tanh, softmax, cross_entropy, binary_cross_entropy
 from .modules import Module, Linear, Conv2D, Conv2DTranspose
 from .optim import SGD, Adam
 
 __all__ = [
-    "Tensor", "_grad_broadcast",
+    "Tensor", 
     "sigmoid", "relu", "tanh", "softmax", "cross_entropy", "binary_cross_entropy",
     "Module", "Linear", "Conv2D", "Conv2DTranspose",
     "SGD", "Adam"
